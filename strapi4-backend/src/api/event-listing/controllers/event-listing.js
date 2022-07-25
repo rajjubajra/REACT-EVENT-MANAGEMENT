3634a5526d027ddb0cff6cd0d2e58e60e5a1c98e
rajjubajra <rajjubajra@gmail.com>
2022-07-25 10:13:35 +0100

    strapi app removed

--- strapi4-backend/src/api/event-listing/controllers/event-listing.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  event-listing controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::event-listing.event-listing');
--- strapi4-backend/src/api/event-listing/routes/event-listing.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * event-listing router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::event-listing.event-listing');
